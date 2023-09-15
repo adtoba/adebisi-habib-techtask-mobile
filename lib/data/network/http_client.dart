a32bfdf93fe68c87038f3f1c93b0915432417164
adtoba <adetoba53@gmail.com>
2023-09-15 11:18:58 +0100

    feat: Implemented a custom http client to handle network requests

---
+++ lib/data/network/http_client.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:tech_task/domain/constants/endpoints.dart';
+
+class CustomHttpClient {
+  late Dio http;
+
+  CustomHttpClient() {
+    this.http = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: Duration(seconds: 30),
+        receiveTimeout: Duration(seconds: 30),
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json"
+        }
+      )
+    )..interceptors.add(
+      QueuedInterceptorsWrapper(
+        onRequest: ((options, handler) {
+          return handler.next(options);
+        }),
+        onError: (e, handler) {
+          return handler.next(e);
+        },
+        onResponse: (res, handler) {
+          return handler.next(res);
+        },
+      )
+    );
+  }
+}
---
+++ lib/domain/constants/endpoints.dart
@@ -0,0 +1,3 @@
+class ApiEndpoints {
+  static String baseUrl = "https://lb7u7svcm5.execute-api.ap-southeast-1.amazonaws.com/dev";
+}
